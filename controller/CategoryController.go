8b61b904dde1457f31683d09e5f974b45ec5902b
baidazhang <xuexianz19@gmail.com>
2023-05-09 17:10:00 +0800

    完成分类 & 文章 模块

---
+++ controller/CategoryController.go
@@ -0,0 +1,82 @@
+package controller
+
+import (
+	"backend_gin/model"
+	"backend_gin/repository"
+	"backend_gin/response"
+	"backend_gin/vo"
+	"github.com/gin-gonic/gin"
+	"log"
+	"strconv"
+)
+
+type ICategoryController interface {
+	RestController
+}
+
+type CategoryController struct {
+	Repository repository.ICategoryRepository
+}
+
+func NewCategoryController() ICategoryController {
+	categoryController := CategoryController{Repository: repository.NewCategoryRepository()}
+	categoryController.Repository.(repository.CategoryRepository).DB.AutoMigrate(model.Category{})
+	return categoryController
+}
+
+func (c CategoryController) Create(ctx *gin.Context) {
+	// 获取参数
+	var requestCategory vo.CreateCategoryRequest
+	if err := ctx.ShouldBind(&requestCategory); err != nil {
+		log.Println(err.Error())
+		response.Fail(ctx, "数据验证错误", nil)
+		return
+	}
+	category, err := c.Repository.Create(requestCategory.Name)
+	if err != nil {
+		response.Fail(ctx, "数据验证错误", nil)
+		return
+	}
+	response.Success(ctx, gin.H{"category": category}, "创建成功")
+}
+
+func (c CategoryController) Update(ctx *gin.Context) {
+	// 获取body 参数
+	var requestCategory vo.CreateCategoryRequest
+	if err := ctx.ShouldBind(&requestCategory); err != nil {
+		log.Println(err.Error())
+		response.Fail(ctx, "数据验证错误", nil)
+		return
+	}
+	// 获取path 参数
+	categoryId, _ := strconv.Atoi(ctx.Params.ByName("id"))
+	var updateCategory *model.Category
+	category, err := c.Repository.SelectById(categoryId)
+	if err != nil {
+		panic(err)
+	}
+	updateCategory, err = c.Repository.Update(*category, requestCategory.Name)
+	if err != nil {
+		panic(err)
+	}
+	response.Success(ctx, gin.H{"category": updateCategory}, "更新成功")
+
+}
+
+func (c CategoryController) Show(ctx *gin.Context) {
+	// 获取path 参数
+	categoryId, _ := strconv.Atoi(ctx.Params.ByName("id"))
+	category, err := c.Repository.SelectById(categoryId)
+	if err != nil {
+		panic(err)
+	}
+	response.Success(ctx, gin.H{"category": category}, "")
+}
+
+func (c CategoryController) Delete(ctx *gin.Context) {
+	// 获取 path 参数
+	categoryId, _ := strconv.Atoi(ctx.Params.ByName("id"))
+	c.Repository.DeleteById(categoryId)
+
+	response.Success(ctx, nil, "删除成功")
+}
